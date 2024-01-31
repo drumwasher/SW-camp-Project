47e2f73bf301e44afb37831f69f73720ef2454a8
drumwasher <tkdlek4186@naver.com>
2024-01-31 17:40:51 +0900

    시뮬링크 보드 실습1

---
+++ 1주차-개별플젝1/새 폴더/gpio_test_ert_rtw/ert_main.c
@@ -0,0 +1,214 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'gpio_test'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+ * C/C++ source code generated on : Wed Jan 31 17:37:27 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "gpio_test.h"
+#include "rtwtypes.h"
+#include "xcp.h"
+#include "ext_mode.h"
+#include "MW_target_hardware_resources.h"
+
+volatile int IsrOverrun = 0;
+boolean_T isRateRunning[2] = { 0, 0 };
+
+boolean_T need2runFlags[2] = { 0, 0 };
+
+void rt_OneStep(void)
+{
+  extmodeSimulationTime_T currentTime = (extmodeSimulationTime_T) 0;
+  boolean_T eventFlags[2];
+
+  /* Check base rate for overrun */
+  if (isRateRunning[0]++) {
+    IsrOverrun = 1;
+    isRateRunning[0]--;                /* allow future iterations to succeed*/
+    return;
+  }
+
+  /*
+   * For a bare-board target (i.e., no operating system), the rates
+   * that execute this base step are buffered locally to allow for
+   * overlapping preemption.
+   */
+  gpio_test_SetEventsForThisBaseStep(eventFlags);
+
+#ifndef _MW_ARDUINO_LOOP_
+
+  sei();
+
+#endif;
+
+  currentTime = (extmodeSimulationTime_T) ((gpio_test_M->Timing.clockTick0 * 1)
+    + 0)
+    ;
+  gpio_test_step0();
+
+  /* Get model outputs here */
+
+  /* Trigger External Mode event */
+  extmodeEvent(0, currentTime);
+
+#ifndef _MW_ARDUINO_LOOP_
+
+  cli();
+
+#endif;
+
+  isRateRunning[0]--;
+  if (eventFlags[1]) {
+    if (need2runFlags[1]++) {
+      IsrOverrun = 1;
+      need2runFlags[1]--;              /* allow future iterations to succeed*/
+      return;
+    }
+  }
+
+  if (need2runFlags[1]) {
+    if (isRateRunning[1]) {
+      /* Yield to higher priority*/
+      return;
+    }
+
+    isRateRunning[1]++;
+
+#ifndef _MW_ARDUINO_LOOP_
+
+    sei();
+
+#endif;
+
+    /* Step the model for subrate "1" */
+    switch (1)
+    {
+     case 1 :
+      currentTime = (extmodeSimulationTime_T) ((gpio_test_M->Timing.clockTick1 *
+        20) + 0)
+        ;
+      gpio_test_step1();
+
+      /* Get model outputs here */
+
+      /* Trigger External Mode event */
+      extmodeEvent(1, currentTime);
+      break;
+
+     default :
+      break;
+    }
+
+#ifndef _MW_ARDUINO_LOOP_
+
+    cli();
+
+#endif;
+
+    need2runFlags[1]--;
+    isRateRunning[1]--;
+  }
+}
+
+extern void rtIOStreamResync();
+volatile boolean_T stopRequested;
+volatile boolean_T runModel;
+int main(void)
+{
+  float modelBaseRate = 0.1;
+  float systemClock = 0;
+  extmodeErrorCode_T errorCode = EXTMODE_SUCCESS;
+
+  /* Initialize variables */
+  stopRequested = false;
+  runModel = false;
+  init();
+  MW_Arduino_Init();
+  rtmSetErrorStatus(gpio_test_M, 0);
+
+  /* Set Final Simulation Time in Ticks */
+  errorCode = extmodeSetFinalSimulationTime((extmodeSimulationTime_T) -1);
+
+  /* Parse External Mode command line arguments */
+  errorCode = extmodeParseArgs(0, NULL);
+  if (errorCode != EXTMODE_SUCCESS) {
+    return (errorCode);
+  }
+
+  gpio_test_initialize();
+  cli();
+  sei();
+
+  /* External Mode initialization */
+  errorCode = extmodeInit(gpio_test_M->extModeInfo, &rtmGetTFinal(gpio_test_M));
+  if (errorCode != EXTMODE_SUCCESS) {
+    /* Code to handle External Mode initialization errors
+       may be added here */
+  }
+
+  if (errorCode == EXTMODE_SUCCESS) {
+    /* Wait until a Start or Stop Request has been received from the Host */
+    extmodeWaitForHostRequest(EXTMODE_WAIT_FOREVER);
+    if (extmodeStopRequested()) {
+      rtmSetStopRequested(gpio_test_M, true);
+    }
+  }
+
+  cli();
+  configureArduinoAVRTimer();
+  runModel = !extmodeSimulationComplete() && !extmodeStopRequested() &&
+    !rtmGetStopRequested(gpio_test_M);
+
+#ifndef _MW_ARDUINO_LOOP_
+
+  sei();
+
+#endif;
+
+  XcpStatus lastXcpState = xcpStatusGet();
+  sei();
+  while (runModel) {
+    /* Run External Mode background activities */
+    errorCode = extmodeBackgroundRun();
+    if (errorCode != EXTMODE_SUCCESS) {
+      /* Code to handle External Mode background task errors
+         may be added here */
+    }
+
+    stopRequested = !(!extmodeSimulationComplete() && !extmodeStopRequested() &&
+                      !rtmGetStopRequested(gpio_test_M));
+    runModel = !(stopRequested);
+    if (stopRequested)
+      disable_rt_OneStep();
+    if (lastXcpState==XCP_CONNECTED && xcpStatusGet()==XCP_DISCONNECTED)
+      rtIOStreamResync();
+    lastXcpState = xcpStatusGet();
+    MW_Arduino_Loop();
+  }
+
+  /* Terminate model */
+  gpio_test_terminate();
+
+  /* External Mode reset */
+  extmodeReset();
+  cli();
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
